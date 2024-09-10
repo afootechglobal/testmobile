d0f27ee0b59bf9342fd38c828e292f2bc52147a7
Afolabi Abayomi <afolabitaiwoabayomi112@gmail.com>
2024-09-10 05:51:32 +0100

    new updated

--- js/scripts.js
+++ js/scripts.js
@@ -232,8 +232,7 @@ function _alertClose3() {
 function _getForm(page) {
 	$("#get-more-div").html('<div class="ajax-loader"><img src="'+websiteUrl+'/all-images/images/ajax-loader.gif"/></div>').fadeIn('fast');
 	$.ajax({
-		type: "POST",
-		url: websiteUrl+'/content/'+page,
+		url: websiteUrl+"/content/"+page,
 		cache: false,
 		success: function (html) {
 			$("#get-more-div").html(html);
